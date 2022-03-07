f7ac264de0d4db0acdb27f51fa19cb6346f31169
liuxueshen429 <1151820475@qq.com>
2022-03-07 14:09:23 -0500

    resolve sdk ver

--- AndroidApp/app/src/main/java/edu/umich/imagician/RequestStatusActivity.kt
+++ AndroidApp/app/src/main/java/edu/umich/imagician/RequestStatusActivity.kt
@@ -1,12 +1,16 @@
 package edu.umich.imagician
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import edu.umich.imagician.databinding.ActivityRequestStatusBinding
 
 class RequestStatusActivity : AppCompatActivity() {
+    private lateinit var view: ActivityRequestStatusBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_request_status)
+        setContentView(view.root)
     }
 
 }
