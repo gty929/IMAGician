c1615f4a95a865deb609b0d35c25edfbe0794944
liuxueshen429 <1151820475@qq.com>
2022-03-14 22:15:26 -0400

    send request message

---
+++ AndroidApp/app/src/main/java/edu/umich/imagician/SendRequestActivity.kt
@@ -0,0 +1,11 @@
+package edu.umich.imagician
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SendRequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_request)
+    }
+}
