b99cf04634e580661ef272176e7933c3f2a94b3c
Puchen Xu <korolxu@umich.edu>
2022-03-17 15:28:37 -0400

    Fix TimeZone Bugs

--- AndroidApp/app/src/main/java/edu/umich/imagician/WatermarkPost.kt
+++ AndroidApp/app/src/main/java/edu/umich/imagician/WatermarkPost.kt
@@ -9,6 +9,8 @@ import retrofit2.Response
 import edu.umich.imagician.Sendable.Mode
 import org.json.JSONObject
 import edu.umich.imagician.ApiStrings.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 // Class of post
 
@@ -94,6 +96,7 @@ open class WatermarkPost (var tag: String? = null,
             phoneNumber = f(PHONE)
             email = f(EMAIL)
             timestamp = f(TIME)
+            TimeExchange()
             checksum = f(CHECKSUM)
             folder = f(FOLDER_NAME)
             folder_pos = f(FOLDER_POS)
@@ -101,6 +104,19 @@ open class WatermarkPost (var tag: String? = null,
 
         } catch (e: Exception) {
             Log.e("UserInfo", "cannot parse JSON string $jsonObjectStr", e)
+        }
+    }
+
+    private fun TimeExchange() {
+        when (timestamp) {
+            null -> timestamp = "Unknown Time"
+            else -> {
+                val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+                formatter.timeZone = TimeZone.getTimeZone("GMT")
+                val dt = formatter.parse(timestamp)
+                val formatter2 = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+                timestamp = formatter2.format(dt)
+            }
         }
     }
 
