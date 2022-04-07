1b2e0ca3e790d76c92477642a225f2cca02a26d2
Tianyao Gu <gty929@sjtu.edu.cn>
2022-04-07 00:02:11 -0400

    unit test

--- AndroidApp/app/src/test/java/edu/umich/imagician/ExampleUnitTest.kt
+++ AndroidApp/app/src/test/java/edu/umich/imagician/ExampleUnitTest.kt
@@ -1,6 +1,8 @@
 package edu.umich.imagician
 
 import android.graphics.Bitmap
+import edu.umich.imagician.utils.ktdecode
+import edu.umich.imagician.utils.ktencode
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -18,7 +20,18 @@ class ExampleUnitTest {
 
     @Test
     fun testEncodeDecode() {
-//        val arr = [0x1111111,0x2222222]
-//        val img:Bitmap = Bitmap.createBitmap()
+        val W = 5
+        val H = 4
+        val img:Bitmap = Bitmap.createBitmap(5, 4, Bitmap.Config.ARGB_8888)
+        for (w in 0 until W) {
+            for (h in 0 until H) {
+                img.setPixel(w, h, (w+h) * 0x10101)
+            }
+        }
+        val encodeImg = ktencode(img, "a")
+        if (encodeImg != null) {
+            val decodeMsg = ktdecode(encodeImg)
+            print(decodeMsg)
+        }
     }
 }
