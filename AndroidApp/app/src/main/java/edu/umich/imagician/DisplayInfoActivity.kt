9c55fd6aa68ff76664ca20924279a46e1c547b3e
liuxueshen429 <1151820475@qq.com>
2022-04-08 18:06:47 -0400

    download to Download

--- AndroidApp/app/src/main/java/edu/umich/imagician/DisplayInfoActivity.kt
+++ AndroidApp/app/src/main/java/edu/umich/imagician/DisplayInfoActivity.kt
@@ -12,6 +12,7 @@ import android.util.Log
 import android.view.*
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
@@ -31,6 +32,32 @@ class DisplayInfoActivity : AppCompatActivity() {
     private lateinit var watermarkPost: WatermarkPost
     private var isModified = false
     private var imageUri: Uri?= null
+
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog. Save the return value, an instance of
+    // ActivityResultLauncher. You can use either a val, as shown in this snippet,
+    // or a lateinit var in your onAttach() or onCreate() method.
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+                downloadFile(
+                    URL("https://3.84.195.179/uploads/${watermarkPost.folder_pos}/"),
+                    "/storage/emulated/0/Download/${watermarkPost.folder}"
+                )
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+                toast("Permission denied")
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -122,26 +149,9 @@ class DisplayInfoActivity : AppCompatActivity() {
             }
             else -> {
                 // You can directly ask for the permission.
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    1)
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
-        when (requestCode) {
-            1 -> {
-                if (grantResults.isNotEmpty() && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED) {
-                    downloadFile(
-                        URL("https://3.84.195.179/uploads/${watermarkPost.folder_pos}/"),
-                        "/storage/emulated/0/Download/${watermarkPost.folder}"
-                    )
-                } else {
-                    toast("Permission denied!")
-                }
-                return
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
             }
         }
     }
